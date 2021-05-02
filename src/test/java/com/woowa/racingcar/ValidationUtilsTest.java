96dd9f21a64b93850be3ecccf7ce30906c2f43d9
jihyeonSong <withsongjihyeon@gmail.com>
2021-05-02 23:22:44 +0900

    test: 자동차 이름 검증
    - 자동차 이름 구분 검증
    - 자동차 이름 길이 검증

--- src/test/java/com/woowa/racingcar/ValidationUtilsTest.java
+++ src/test/java/com/woowa/racingcar/ValidationUtilsTest.java
@@ -6,11 +6,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ValidationUtilsTest {
-
+	
+	@Test
+	@DisplayName(value = "자동차_이름_구분_검증")
+	void 자동차_이름_구분_검증() {
+		assertThat(ValidationUtils.validCarNameSeparator("A")).isEqualTo(1);
+		assertThat(ValidationUtils.validCarNameSeparator("A,B,C,D")).isEqualTo(4);
+		assertThat(ValidationUtils.validCarNameSeparator("A,B,C,")).isEqualTo(3);
+		assertThat(ValidationUtils.validCarNameSeparator(",")).isEqualTo(0);
+		assertThat(ValidationUtils.validCarNameSeparator(",,")).isEqualTo(0);
+		assertThat(ValidationUtils.validCarNameSeparator(",,,")).isEqualTo(0);
+		assertThat(ValidationUtils.validCarNameSeparator("")).isEqualTo(0);
+	}
+	
 	@Test
 	@DisplayName(value = "자동차_이름_길이_검증")
 	void 자동차_이름_길이_검증() {
-		assertThat( ValidationUtils.validCarNameLength("MYCAR")).isTrue();
-		assertThat( ValidationUtils.validCarNameLength("C")).isTrue();
+		assertThat(ValidationUtils.validCarNameLength("MYCAR")).isTrue();
+		assertThat(ValidationUtils.validCarNameLength("M")).isTrue();
+		assertThat(ValidationUtils.validCarNameLength("MYGOODCAR")).isFalse();
+		assertThat(ValidationUtils.validCarNameLength("")).isFalse();
+		assertThat(ValidationUtils.validCarNameLength("  ")).isFalse();
 	}
 }
