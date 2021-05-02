ffe7f7326e073a10d465a67ab0ae53667924a9a9
jihyeonSong <withsongjihyeon@gmail.com>
2021-05-02 23:23:50 +0900

    feature: 자동차 이름 검증 유틸리티 개발
    - 자동차 이름 구분 검증
    - 자동차 이름 길이 검증

--- src/main/java/com/woowa/racingcar/ValidationUtils.java
+++ src/main/java/com/woowa/racingcar/ValidationUtils.java
@@ -1,12 +1,25 @@
 package com.woowa.racingcar;
 
+import java.util.Arrays;
+
 public class ValidationUtils {
+	private static String CAR_NAME_SEPARATOR = ",";
 	private static int MAX_CAR_NAME_LENGTH = 5;
 	private static int MIN_CAR_NAME_LENGTH = 1;
-
-	public static boolean validCarNameLength(String carName) {
-		return carName.length() >= MIN_CAR_NAME_LENGTH
-				&& carName.length() <= MAX_CAR_NAME_LENGTH;
+	
+	public static int validCarNameSeparator(String carNames) {
+		if (carNames.length() == 0) {
+			return 0;
+		}
+		return Arrays.asList(carNames.split(CAR_NAME_SEPARATOR)).size();
 	}
+	
+	public static boolean validCarNameLength(String carName) {
+		int carNameLength = carName.trim().length();
+		
+		return (carNameLength >= MIN_CAR_NAME_LENGTH && carNameLength <= MAX_CAR_NAME_LENGTH);
+	}
+
+
 
 }
