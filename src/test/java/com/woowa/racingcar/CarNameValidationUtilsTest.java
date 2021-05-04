a44b5c559548f23d1ea0041d53a2e3342a2692d2
jihyeonSong <withsongjihyeon@gmail.com>
2021-05-04 23:30:55 +0900

    feat: 자동차 이름 검증 테스트

--- src/test/java/com/woowa/racingcar/CarNameValidationUtilsTest.java
+++ src/test/java/com/woowa/racingcar/CarNameValidationUtilsTest.java
@@ -5,27 +5,29 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ValidationUtilsTest {
+import com.woowa.utils.CarNameValidationUtils;
+
+public class CarNameValidationUtilsTest {
 	
 	@Test
 	@DisplayName(value = "자동차_이름_구분_검증")
 	void 자동차_이름_구분_검증() {
-		assertThat(ValidationUtils.validCarNameSeparator("A")).isEqualTo(1);
-		assertThat(ValidationUtils.validCarNameSeparator("A,B,C,D")).isEqualTo(4);
-		assertThat(ValidationUtils.validCarNameSeparator("A,B,C,")).isEqualTo(3);
-		assertThat(ValidationUtils.validCarNameSeparator(",")).isEqualTo(0);
-		assertThat(ValidationUtils.validCarNameSeparator(",,")).isEqualTo(0);
-		assertThat(ValidationUtils.validCarNameSeparator(",,,")).isEqualTo(0);
-		assertThat(ValidationUtils.validCarNameSeparator("")).isEqualTo(0);
+		assertThat(CarNameValidationUtils.validCarNameSeparator("A")).isEqualTo(1);
+		assertThat(CarNameValidationUtils.validCarNameSeparator("A,B,C,D")).isEqualTo(4);
+		assertThat(CarNameValidationUtils.validCarNameSeparator("A,B,C,")).isEqualTo(3);
+		assertThat(CarNameValidationUtils.validCarNameSeparator(",")).isEqualTo(0);
+		assertThat(CarNameValidationUtils.validCarNameSeparator(",,")).isEqualTo(0);
+		assertThat(CarNameValidationUtils.validCarNameSeparator(",,,")).isEqualTo(0);
+		assertThat(CarNameValidationUtils.validCarNameSeparator("")).isEqualTo(0);
 	}
 	
 	@Test
 	@DisplayName(value = "자동차_이름_길이_검증")
 	void 자동차_이름_길이_검증() {
-		assertThat(ValidationUtils.validCarNameLength("MYCAR")).isTrue();
-		assertThat(ValidationUtils.validCarNameLength("M")).isTrue();
-		assertThat(ValidationUtils.validCarNameLength("MYGOODCAR")).isFalse();
-		assertThat(ValidationUtils.validCarNameLength("")).isFalse();
-		assertThat(ValidationUtils.validCarNameLength("  ")).isFalse();
+		assertThat(CarNameValidationUtils.validCarNameLength("MYCAR")).isTrue();
+		assertThat(CarNameValidationUtils.validCarNameLength("M")).isTrue();
+		assertThat(CarNameValidationUtils.validCarNameLength("MYGOODCAR")).isFalse();
+		assertThat(CarNameValidationUtils.validCarNameLength("")).isFalse();
+		assertThat(CarNameValidationUtils.validCarNameLength("  ")).isFalse();
 	}
 }
