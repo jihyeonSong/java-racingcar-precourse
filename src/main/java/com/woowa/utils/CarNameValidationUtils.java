2e24fb0a2984ca068099fbb9f3169f479bfb995e
jihyeonSong <withsongjihyeon@gmail.com>
2021-05-04 23:31:04 +0900

    feat: 자동차 이름 검증

--- src/main/java/com/woowa/utils/CarNameValidationUtils.java
+++ src/main/java/com/woowa/utils/CarNameValidationUtils.java
@@ -1,25 +1,22 @@
-package com.woowa.racingcar;
+package com.woowa.utils;
 
 import java.util.Arrays;
 
-public class ValidationUtils {
-	private static String CAR_NAME_SEPARATOR = ",";
-	private static int MAX_CAR_NAME_LENGTH = 5;
-	private static int MIN_CAR_NAME_LENGTH = 1;
-	
+public class CarNameValidationUtils {
+	private static final String CAR_NAME_SEPARATOR = ",";
+	private static final int MAX_CAR_NAME_LENGTH = 5;
+	private static final int MIN_CAR_NAME_LENGTH = 1;
+
 	public static int validCarNameSeparator(String carNames) {
 		if (carNames.length() == 0) {
 			return 0;
 		}
 		return Arrays.asList(carNames.split(CAR_NAME_SEPARATOR)).size();
 	}
-	
+
 	public static boolean validCarNameLength(String carName) {
 		int carNameLength = carName.trim().length();
-		
+
 		return (carNameLength >= MIN_CAR_NAME_LENGTH && carNameLength <= MAX_CAR_NAME_LENGTH);
 	}
-
-
-
 }
